7e32e24d04f3b7b1ac3ee15efd2c26f724bd6be1
Kat <katweeddale@gmail.com>
2022-07-15 11:01:59 +0100

    Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import ArticlesArea from "./Components/ArticlesArea/ArticlesArea";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <ArticlesArea />
     </div>
   );
