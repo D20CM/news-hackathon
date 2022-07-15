9ab0fe3b8e0a8a47189fe5af9ffb195d30415273
D20CM <scottbrown500@hotmail.com>
2022-07-15 11:31:29 +0100

    add article image and basic styling

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -1,8 +1,16 @@
 import React from "react";
+import styles from "./article.module.css";
 
 function Article({ article }) {
   return (
-    <div>
+    <div className={styles.articleCard}>
+      {article.image_url !== null && (
+        <img
+          src={article.image_url}
+          alt={article.title}
+          className={styles.articleImage}
+        ></img>
+      )}
       <h2>{article.title}</h2>
       <p>{article.title}</p>
     </div>
