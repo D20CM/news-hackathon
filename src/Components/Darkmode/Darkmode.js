4463e60115eb2c9d4f431dd6118ba81b57fe1643
Kat <katweeddale@gmail.com>
2022-07-15 14:43:34 +0100

    darkmode

--- src/Components/Darkmode/Darkmode.js
+++ src/Components/Darkmode/Darkmode.js
@@ -1,9 +1,14 @@
 import styles from "./darkmode.module.css";
 import image from "./107753.png";
 
-export default function Darkmode() {
+export default function Darkmode(dark, setDark) {
   return (
-    <div className={styles.darkmode}>
+    <div
+      className={styles.darkmode}
+      onClick={() => {
+        setDark(!dark);
+      }}
+    >
       <img className={styles.img} alt="dark mode" src={image}></img>
     </div>
   );
