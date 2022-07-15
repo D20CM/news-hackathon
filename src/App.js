729996690eacedb44f719a255a3b074081d9940d
Kat <katweeddale@gmail.com>
2022-07-15 13:04:15 +0100

    menus

--- src/App.js
+++ src/App.js
@@ -2,25 +2,23 @@ import ArticlesArea from "./Components/ArticlesArea/ArticlesArea";
 import Header from "./Components/Header/Header";
 import { useState, useEffect } from "react";
 import "./App.css";
-import Article from "./Components/Article/Article";
 
 function App() {
   const [articles, setArticles] = useState([]);
 
   const url = `https://newsdata.io/api/1/news?apikey=${process.env.REACT_APP_API_KEY}&country=gb&language=en`;
 
-  async function getArticles() {
-    const response = await fetch(url);
-    const data = await response.json();
-    const results = data.results;
-    console.log(results);
-
-    setArticles(results);
-  }
-
   useEffect(() => {
+    async function getArticles() {
+      const response = await fetch(url);
+      const data = await response.json();
+      const results = data.results;
+      console.log(results);
+
+      setArticles(results);
+    }
     getArticles();
-  }, []);
+  }, [url]);
 
   return (
     <div className="App">
